cc18903a427221523fc565256ed36cd6d984aef6
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:38:47 +0300

    added task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * main - start
+ * Return:0
+*/
+
+int main(void)
+{
+	int summ = 0, i;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			summ += i;
+	}
+
+
+	printf("%d", summ);
+}
