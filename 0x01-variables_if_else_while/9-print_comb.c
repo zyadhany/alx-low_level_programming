7ef9a570e9e854ce370e71e5a67984ea64a80d77
zyadhany <zeyad.hany2003@gmail.com>
2023-07-22 19:17:19 +0300

    added task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,15 @@ int main(void)
 {
 
 char a;
-for (a = '0'; a <= '8'; a++)
+for (a = '0'; a <= '9'; a++)
 {
 putchar(a);
+if (a < '9')
+{
 putchar(' ');
 putchar(',');
-
 }
-putchar(a);
+}
 
 putchar('\n');
 
