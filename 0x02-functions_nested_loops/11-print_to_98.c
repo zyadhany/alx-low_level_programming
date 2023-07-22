972954d4e12eb08d626ef1becf41aa533ba1dc5e
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:24:03 +0300

    added task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,10 +10,13 @@
 
 void print_to_98(int n)
 {
-	printf("%d", n);
-	for (n = n + 1; n <= 98; n++)
+	while (n != 98)
 	{
-		printf(", %d", n);
+		printf("%d, ", n);
+		if (n > 98)
+			n--;
+		else
+		n++;
 	}
-	putchar('\n');
+	printf("%d\n", n);
 }
