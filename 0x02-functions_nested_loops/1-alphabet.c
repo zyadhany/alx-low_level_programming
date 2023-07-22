8fda4a8b5ac285110f7862bc4d0b95ccb4393d87
zyadhany <zeyad.hany2003@gmail.com>
2023-07-22 22:50:20 +0300

    added task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,12 +9,7 @@
 
 int main(void)
 {
-char a;
-for (a = 'a'; a <= 'z'; a++)
-{
-putchar(a);
-}
-putchar('\n');
+print_alphabet();
 return (0);
 }
 
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,9 @@
+
+void print_alphabet(){
+char a;
+for (a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+}
