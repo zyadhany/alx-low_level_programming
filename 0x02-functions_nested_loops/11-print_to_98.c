bc5ca45238ee671c2ffb483a125a19a53f399d38
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:22:13 +0300

    added task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * print_to_98 - start
+ * @n: the input to be cheaked
+ * Return:0
+*/
+
+void print_to_98(int n)
+{
+	printf("%d", n);
+	for (n = n + 1; n <= 98; n++)
+	{
+		printf(", %d", n);
+	}
+	putchar('\n');
+}
