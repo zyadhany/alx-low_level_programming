c271478c810dddb94fcd2753c039ef8a4dde5f57
zyadhany <zeyad.hany2003@gmail.com>
2023-07-22 16:33:20 +0300

    added task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return:0
+*/
+
+int main(void)
+{
+
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
+
+}
