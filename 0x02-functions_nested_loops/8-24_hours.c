41f12cdb7eddfa1ee6cf0406136be0876a53c950
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:09:16 +0300

    added task

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * print_last_digit - start
+ * Return:0
+*/
+
+void jack_bauer(void)
+{
+	int a, b, c, d, f = '9';
+
+	for (a = '0'; a <= '2'; a++)
+	{
+		if (a == '2')
+		{
+			f = '4';
+		}
+
+		for (b = '0'; b <= f; b++)
+		{
+			for (c = '0'; c < '6'; c++)
+			{
+				for (d = 0; d <= '9'; d++)
+				{
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+
+}
