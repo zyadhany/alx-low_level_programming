73062bdd55233b4cfb6e352ba69650d7f4bc4e2d
zyadhany <zeyad.hany2003@gmail.com>
2023-07-22 23:52:09 +0300

    added task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * int print_last_digit(int); - start
+ * @n: the input to be cheaked
+ * Return:0
+*/
+
+int print_last_digit(int n)
+{
+	_putchar('0' + n % 10);
+	return (n % 10);
+}
