7416f1c0ea3dedf1c5379bc91411fc858b629cdb
zyadhany <zeyad.hany2003@gmail.com>
2023-07-22 19:43:44 +0300

    added task

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return:0
+*/
+
+int main(void)
+{
+
+int a, b, c, d;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = a + 1; b <= '9'; b++)
+{
+for (c = a; c <= '9'; c++)
+{
+for (d = b + 1; c <= '9'; c++)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(c);
+if (a != '9' || b != '8' || c != '9' || d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+
+}
