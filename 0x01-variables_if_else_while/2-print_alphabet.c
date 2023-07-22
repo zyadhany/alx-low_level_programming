c73de3aee4a881fb518ffa616bbcc27cd256c89d
zyadhany <zeyad.hany2003@gmail.com>
2023-07-22 16:24:46 +0300

    added task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - start
+ * Return:0
+*/
+
+int main(void)
+{
+
+
+for (char a = 'a'; a <= 'z'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
+
+}
