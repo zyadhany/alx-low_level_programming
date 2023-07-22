04f6239b59654f0b43d7c7263ca1d3fc7fa4fcd5
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:31:14 +0300

    added task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * print_times_table - start
+ * @n: the input to be cheaked
+ * Return:0
+*/
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	for (i = 0; i <= n; i++)
+	{
+		putchar('0');
+		for (j = 1; j <= n; j++)
+		{
+			int x = i * j, k = 10000;
+			
+			putchar(',');
+			while (k > x)
+			{
+				putchar(' ');
+				k /= 10;
+			}
+			printf('%d', x);
+		}
+		putchar('\n');
+	}
+}
