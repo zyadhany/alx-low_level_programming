f7e139c513ff520c6752a67c14b786d7a6db9d7f
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:17:03 +0300

    added task

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * add - start
+ * @a: the input to be cheaked
+ * @b: the input to be cheaked
+ * Return:0
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
