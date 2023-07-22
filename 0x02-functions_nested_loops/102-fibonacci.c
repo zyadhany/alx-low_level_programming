640ee4fe67dfe22005f51af08ff6bd8b31dbf185
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 00:44:17 +0300

    added task

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
 
+/**
+ * main - start
+ * Return:0
+*/
+
+int main(void)
+{
+	long int fib[60] = {0}, i;
+
+    fib[0] = 1;
+    fib[1] = 1;
+	for (i = 0; i <= 50; i++)
+	{
+		fib[i + 1] += fib[i];
+		fib[i + 2] += fib[i];
+	}
+    putchar('1');
+    for (i = 1; i <= 50; i++)
+	{
+		printf(", %ld", fib[i]);
+	}
+    putchar('\n')
+return (0);
+}
