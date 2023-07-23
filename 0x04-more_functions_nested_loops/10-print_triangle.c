234a249c2ec24edbd1d34f6650f29d37b53e42ae
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 22:05:38 +0300

    added task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * print_triangle - start
+ * @size: the input char
+ * Return:0
+*/
+
+void print_triangle(int size)
+{
+	int i, j, n = size;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n - i; j++)
+		{
+			_putchar(' ');
+		}
+		for (j = n - i; j <= n; j++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+
+	if (n <= 0)
+		_putchar('\n');
+}
