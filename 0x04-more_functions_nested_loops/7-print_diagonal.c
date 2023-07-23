e93579c8dc24a2018f0112f88e05805e18c9adad
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 21:23:05 +0300

    added task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,11 +14,12 @@ void print_diagonal(int n)
 {
 	int i = 0;
 
-	for (i = 1; i < n; i++)
+	for (i = 1; i <= n; i++)
 	{
+		if (i != n)
 		_putchar(' ');
+		else
+		_putchar('\\');
 	}
-	if (i == n)
-	_putchar('\\');
 	_putchar('\n');
 }
