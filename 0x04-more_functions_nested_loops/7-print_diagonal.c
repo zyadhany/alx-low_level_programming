30c67df196f154d7adea8db19918c724023add47
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 21:18:22 +0300

    added task

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * print_diagonal - start
+ * @n: the input char
+ * Return:0
+*/
+
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		_putchar(' ');
+	}
+	if (i != n)
+	_putchar('\\');
+	_putchar('\n');
+}
