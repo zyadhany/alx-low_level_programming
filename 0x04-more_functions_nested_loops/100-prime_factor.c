9e6d4c587709cdcd899e1d425c97ca9a62a17e55
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 21:31:35 +0300

    added task

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * main - start
+ * Return:0
+*/
+
+int main(void)
+{
+	long int i, j, mx = 0, n = 612852475143;
+
+	for (i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			n /= i;
+			mx = i;
+		}
+	}
+	if (n > mx)
+		mx = n;
+
+	printf("%ld\n", mx);
+
+	return (0);
+}
