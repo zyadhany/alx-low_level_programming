db5849eac837614bb02fd450f53f5e557134c354
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 21:38:36 +0300

    added task

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * print_square - start
+ * @size: the input char
+ * Return:0
+*/
+
+void print_square(int size)
+{
+	int i, j, n = size;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (j = 1; j <= n; j++)
+		{
+			_putchar('#');
+		}
+	_putchar('\n');
+	}
+
+}
