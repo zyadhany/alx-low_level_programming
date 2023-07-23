1673306d01cc6f0c30ffbd18d6eadb151b3c2d05
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 14:19:20 +0300

    added task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,52 +1,29 @@
 #include "main.h"
 
 /**
-* convert_day - converts day of month to day of year, without accounting
-* for leap year
-* @month: month in number format
-* @day: day of month
-* Return: day of year
-*/
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 
-int convert_day(int month, int day)
+int largest_number(int a, int b, int c)
 {
-	switch (month)
+	int largest;
+
+	if (a >= b && a >= c)
 	{
-		case 2:
-			day = 31 + day;
-			break;
-		case 3:
-			day = 59 + day;
-			break;
-		case 4:
-			day = 90 + day;
-			break;
-		case 5:
-			day = 120 + day;
-			break;
-		case 6:
-			day = 151 + day;
-			break;
-		case 7:
-			day = 181 + day;
-			break;
-		case 8:
-			day = 212 + day;
-			break;
-		case 9:
-			day = 243 + day;
-			break;
-		case 10:
-			day = 273 + day;
-			break;
-		case 11:
-			day = 304 + day;
-			break;
-		case 12:
-			day = 334 + day;
-			break;
-		default:
-			break;
+		largest = a;
 	}
-	return (day);
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
 }
