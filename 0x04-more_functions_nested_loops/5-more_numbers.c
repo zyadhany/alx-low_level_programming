83b18cddbe50cf0397800d011d23550bcf1aef8b
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 20:37:32 +0300

    added task

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * more_numbers - start
+ * Return:0
+*/
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			int c = (b / 10) % 10;
+
+			if (c)
+				_putchar(c + '0');
+			_putchar(b % 10 + '0');
+		}
+	}
+	_putchar('\n');
+}
