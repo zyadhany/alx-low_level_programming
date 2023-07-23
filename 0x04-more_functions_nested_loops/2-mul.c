ffa051649557792d4eb0a7a5721df85b4910ef73
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 20:19:28 +0300

    added task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * _isdigit - start
+ * @a: the input char
+ * @b: the input char
+ * Return:0
+*/
+
+int _isdigit(int a, int b)
+{
+	return (a * b);
+}
