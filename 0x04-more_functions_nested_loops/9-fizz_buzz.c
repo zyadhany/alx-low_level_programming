896f7d9a0803ca55c87b410ad7e7690c8ba5736a
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 21:45:04 +0300

    added task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,39 @@
+#include <stdio.h>
+#include <math.h>
 
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, j, mx = 0;
+
+	putchar('1');
+	for (i = 1; i <= 100; i++)
+	{
+		j = 1;
+		putchar(' ');
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			j = 0;
+		}
+
+		if (i % 5 == 0)
+		{
+			printf("Buzz");
+			j = 0;
+		}
+
+		if (j)
+		{
+			printf("%d", i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
