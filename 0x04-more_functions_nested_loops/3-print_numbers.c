47867976ebdec0e81e41a83b59ff7dcb32c48c16
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 20:22:12 +0300

    added task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * print_numbers - start
+ * Return:0
+*/
+
+void print_numbers(void)
+{
+	char a;
+
+	for (a = 0; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
