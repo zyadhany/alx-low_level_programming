d8614900f51c9c38a59905bc2ead53a5a2075e58
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 21:10:43 +0300

    added task

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * print_line - start
+ * @c: the input char
+ * Return:0
+*/
+
+print_line(int c)
+{
+	int i;
+
+	for (i = 0; i < c; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
