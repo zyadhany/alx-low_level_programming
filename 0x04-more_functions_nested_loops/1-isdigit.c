fd03dc9437ebf1b0982207fd88ba4ccc71de0bbf
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 20:18:25 +0300

    added task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
 
+/**
+ * _isdigit - start
+ * @c: the input char
+ * Return:0
+*/
+
+int _isdigit(int c)
+{
+	return (c <= '9' && c >= '0');
+}
