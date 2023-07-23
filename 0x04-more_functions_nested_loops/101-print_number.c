6659a8f780a548fedaf574280095c6cc9b0ecc3f
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 22:11:50 +0300

    added task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,25 @@
+#include "main.h"
 
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int k;
+
+	if (n < 0)
+	{
+		k = -n;
+		_putchar('-');
+	} else
+		k = n;
+
+	if (k / 10)
+	{
+		print_number(k / 10);
+	}
+
+	_putchar((k % 10) + '0');
+}
