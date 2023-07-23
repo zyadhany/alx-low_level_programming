09546e0cff35a9a8e308db522db43c804b9e6b87
zyadhany <zeyad.hany2003@gmail.com>
2023-07-23 20:14:13 +0300

    added task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * _isupper - start
+ * @c: the input char
+ * Return:0
+*/
+
+int _isupper(int c)
+{
+	return (c <= 'z');
+}
