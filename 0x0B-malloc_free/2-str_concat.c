71885dd345d5e9ccf66079bd82990c0fba80be06
zyadhany <zeyad.hany2003@gmail.com>
2023-08-07 23:46:14 +0300

    added task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Entry point
+ *@s1: input string
+ *@s2: second input string
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+	int i, n = 0, m = 0;
+
+	if (s1)
+	{
+		n = strlen(s1);
+	}
+
+	if (s2)
+	{
+		m = strlen(s2);
+	}
+
+	char *arr = malloc(sizeof(char) * (n + m + 1));
+
+	if (arr)
+	{
+		for (i = 0; i < n; i++)
+		{
+			arr[i] = s1[i];
+		}
+		for (i = 0; i <= m; i++)
+		{
+			arr[n + i] = s1[i];
+		}
+	}
+	if (arr == NULL)
+	{
+		printf("failed to allocate memory\n");
+
+	}
+
+	return (arr);
+}
