f53eefb2d31a4e88405332d3ac76dbc28496b98b
zyadhany <zeyad.hany2003@gmail.com>
2023-08-08 00:07:02 +0300

    added task

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - Entry point
+ *@m: input width
+ *@n: input height
+ * Return: a pointer to the array, or NULL if it fails
+ */
+
+int **alloc_grid(int m, int n)
+{
+	int **arr, i, j;
+
+	*arr = malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++)
+	{
+		arr[i] = malloc(sizeof(int) * m);
+		for (j = 0; j < m; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
