06fe3be8387f6f7f9a5346270f430174932b4045
zyadhany <zeyad.hany2003@gmail.com>
2023-08-08 01:27:14 +0300

    added task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * strtow - Entry point
+ *@str: input string
+ * Return: a pointer to a new string
+ */
+
+char **strtow(char *str)
+{
+	int i, w = 1, j, k = 0, h, re, m, wordf;
+	char **arr;
+	char *x;
+
+	if (*str == '\0' || str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && str[i + 1] != ' ')
+			w++;
+	}
+	arr = malloc(w * sizeof(char *));
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		re = 0;
+		if (str[i] != ' ')
+		{
+			for (j = wordf ; str[j] != '\0' && str[i] != ' '; j++)
+			{
+				re++;
+			}
+			arr[k] = malloc((re + 1) * sizeof(char));
+			if (arr[k] == NULL)
+			{
+				for (h = 0; h <= k; h++)
+					free(arr[h]);
+				free(arr);
+				return (NULL);
+			}
+			for (h = 0; i < j; i++, h++)
+				arr[k][h] = str[i];
+			arr[k][h] = '\0';
+			k++;
+		}
+	}
+	return (arr);
+}
