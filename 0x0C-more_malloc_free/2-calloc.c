d7b8d5988f01302c1d8f9d83d88cd1621541e192
zyadhany <zeyad.hany2003@gmail.com>
2023-08-12 00:33:57 +0300

    added task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+
+/**
+ * *_calloc - intery point
+ * @nmemb: input 1
+ * @size: iput 3
+ * Return: value of functiuon
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *X;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	X = malloc(nmemb * size);
+
+	if (X == NULL)
+	{
+		return (NULL);
+	}
+
+
+	return (X);
+}
