76bcee202a48af982735a8f419fd9b6680c6f1a0
zyadhany <zeyad.hany2003@gmail.com>
2023-08-15 01:33:09 +0300

    added task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,12 @@ struct dog
 	float age;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
