d7776f84730dfa49c47f9beb78245f06493ab450
zyadhany <zeyad.hany2003@gmail.com>
2023-08-15 02:07:53 +0300

    added task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,9 +13,8 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, j, k;
-	dog_t *x;
+	dog_t *x = malloc(sizeof(dog_t));
 
-	x = malloc(sizeof(dog_t));
 	if (!x)
 	{
 		return (NULL);
@@ -32,24 +31,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!x->name || !x->owner)
 	{
-		free(p->name);
-		free(p->owner);
-		free(p);
+		free(x->name);
+		free(x->owner);
+		free(x);
 		return (NULL);
 	}
 
 	for (k = 0; k < i; k++)
 	{
-		p->name[k] = name[k];
+		x->name[k] = name[k];
 	}
-	p->name[k] = '\0';
+	x->name[k] = '\0';
 	for (k = 0; k < j; k++)
 	{
-		p->owner[k] = owner[k];
+		x->owner[k] = owner[k];
 	}
-	p->owner[k] = '\0';
+	x->owner[k] = '\0';
 
-	p->age = age;
+	x->age = age;
 
 	return (p);
 }
