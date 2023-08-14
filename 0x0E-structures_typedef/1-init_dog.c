23f7a35d7a6e58222cb1eb8bea963ed5f858ac53
zyadhany <zeyad.hany2003@gmail.com>
2023-08-15 01:31:11 +0300

    added task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - intery
+ * @d: d
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
