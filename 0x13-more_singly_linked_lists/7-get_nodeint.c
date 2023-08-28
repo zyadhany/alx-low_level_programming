2bc566a2eaa3c5318aa0a4b287253afe29cc1d66
zyadhany <zeyad.hany2003@gmail.com>
2023-08-28 18:51:53 +0300

    added task

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - check the code
+ * @X: given list
+ * @k: index of list
+ *
+ * Return: pointer to list.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *X, unsigned int k)
+{
+
+	int i = 0;
+
+	for (size_t i = 0; i <= k; i++)
+	{
+		if (!X)
+			return (NULL);
+		if (i == k)
+			return (X);
+		X = X->next;
+	}
+
+	return (NULL);
+}
