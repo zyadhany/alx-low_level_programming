d0481acbd738b5dc40f828330e292b25c6eb9ea0
zyadhany <zeyad.hany2003@gmail.com>
2023-08-30 00:57:04 +0300

    added task

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,7 +4,7 @@
 #include "lists.h"
 
 /**
- * print_listint - check the code
+ * print_listint_safe - check the code
  * @X: given list
  *
  * Return: size of list.
@@ -14,18 +14,19 @@ size_t print_listint_safe(const listint_t *X)
 {
 	int n = 0, at = 0, keep = 0, i;
 	listint_t *Y[1024];
+
 	while (X)
 	{
-		for ( i = 0; i < at; i++)
+		for (i = 0; i < at; i++)
 		{
 			if (Y[i] == X->next)
 			{
 				keep = 1;
 				break;
 			}
-			
+
 		}
-		
+
 		if (keep)
 			break;
 		printf("%d\n", X->n);
