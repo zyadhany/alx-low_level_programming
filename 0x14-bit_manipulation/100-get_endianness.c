999413cf0d99ff82cec09f420602ccd86f6b3637
zyadhany <zeyad.hany2003@gmail.com>
2023-09-01 01:21:57 +0300

    added task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include "main.h"
+
+/**
+ * get_endianness - check the code
+ *
+ * Return: pointer to list
+ */
+
+int get_endianness(void)
+{
+	int n = 1;
+
+
+
+	return (*(char *) &n);
+}
