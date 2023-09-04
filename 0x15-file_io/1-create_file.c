8559765d5802a555a119c95f01a72fb9d7449bba
zyadhany <zeyad.hany2003@gmail.com>
2023-09-04 13:23:57 +0300

    added task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,8 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (0);
 
-	for (n = 0; text_content[n] != '\0'; n++)
-	;
+	if (text_content != NULL)
+		for (n = 0; text_content[n] != '\0'; n++)
+		;
 
 
 	fn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
