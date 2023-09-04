2d95c6ee1b956212abe4e76800b4d7a8bb377996
zyadhany <zeyad.hany2003@gmail.com>
2023-09-04 12:50:32 +0300

    added task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,23 +11,30 @@
 
 ssize_t read_textfile(const char *filename, size_t n)
 {
-	ssize_t r, fn, w;
-	char *buff;
+	FILE *fn;
+	ssize_t k = 0;
+	size_t i;
+	char c, *buff;
 
 	buff = malloc(sizeof(char) * n);
 
 	if (!filename || !buff)
 		return (0);
-	fn = open(filename, O_RDONLY);
+	fn = fopen(filename, "r");
 	if (!fn)
 		return (0);
 
 
-	r = read(fn, buff, n);
-	w = write(1, buff, r);
+	for (i = 0; i < n && (c = fgetc(fn)) != EOF; i++)
+	{
+		buff[k] = c;
+		k++;
+	}
+
+	write(1, buff, k);
 
 
 	free(buff);
-	close(fn);
-	return (w);
+	fclose(fn);
+	return (k);
 }
