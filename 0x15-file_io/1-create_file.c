ae721ac97d51599840dea86f9dc7edccec88e9a2
zyadhany <zeyad.hany2003@gmail.com>
2023-09-04 13:20:40 +0300

    added task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+
+/**
+ * create_file - print text of file
+ * @filename: the file name
+ * @text_content: text to write
+ *
+ * Return: pointer to list
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int w, fn, n;
+
+	if (!filename)
+		return (0);
+
+	for (n = 0; text_content[n] != '\0'; n++)
+	;
+
+
+	fn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fn == -1)
+		return (-1);
+
+
+	w = write(fn, text_content, n);
+
+	if (w == -1)
+	{
+		return (-1);
+	}
+
+
+	return (1);
+}
