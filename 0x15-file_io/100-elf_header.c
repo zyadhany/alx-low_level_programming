8d718c0d877b866a1f8c0461411cc3be55df1eaa
zyadhany <zeyad.hany2003@gmail.com>
2023-09-04 14:31:35 +0300

    added task

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,36 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * main - check the code
+ * @ac: num of arg
+ * @av: arr of arg
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) ac, char *av[])
+{
+	int fn, r;
+
+	fn = open(av[1], O_RDONLY);
+	if (fn == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
+
+
+	if (close(fn) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fn);
+		exit(98);
+	}
+	return (0);
+}
