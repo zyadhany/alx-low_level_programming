9dc97d3a572f227c44f459e21d1bdb395c2531b4
zyadhany <zeyad.hany2003@gmail.com>
2023-10-12 19:53:27 +0300

    added task

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all the elements of list
+ * dlistint_len - get numper of element in given list
  * @h: given list
  *
  * Return: numper of element in list
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
 
 	while (h)
 	{
-		printf("%d\n", h->n);
 		n++;
 		h = h->next;
 	}
