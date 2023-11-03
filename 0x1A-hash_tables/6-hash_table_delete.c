3531970d1f4f664ef9ac2ecc82f76233b75492e5
zyadhany <zeyad.hany2003@gmail.com>
2023-11-03 08:20:26 +0200

    added task

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * free_list - delete hash node.
+ * @node: give node.
+ *
+ * Return: Nothing.
+ */
+void free_list(hash_node_t *node)
+{
+	if (!node)
+		return;
+
+	free(node->key);
+	free(node->value);
+	free_list(node->next);
+	free(node);
+}
+
+/**
+ * hash_table_delete - delete hash table.
+ * @ht: hash table.
+ *
+ * Return: Nothing.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i, isit = 0;
+	hash_node_t *tmp;
+
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+		free(ht->array[i]);
+	free(ht->array);
+	free(ht);
+}
