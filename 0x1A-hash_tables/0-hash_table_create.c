f2de591aacff86cbcb4e1e7ba3bd5e2d7efca7a1
zyadhany <zeyad.hany2003@gmail.com>
2023-11-03 07:37:17 +0200

    added task

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *ht;
 
-	ht = malloc(sizeof(hash_node_t));
+	ht = malloc(sizeof(hash_table_t));
 
 	if (!ht)
 		return (NULL);
