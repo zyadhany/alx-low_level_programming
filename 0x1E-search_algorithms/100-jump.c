90231b6a8ad96e89d69451a79d61de0da3ecafb0
zyadhany <zeyad.hany2003@gmail.com>
2024-04-14 14:00:52 +0200

    solved task

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - doing linear search
+ * @array: pointer to array
+ * @size: size of array
+ * @value: value to search
+ *
+ * Return: the index found or -1 if not found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, j, m;
+
+	if (!array)
+		return (-1);
+
+	i = 0, m = sqrt(size);
+
+	if (array[i] > value)
+		return (-1);
+
+	while (i + m < size && array[i + m] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i += m;
+	}
+
+	printf("Value found between indexes [%lu] and [%lu]\n", i, i + m);
+	j = i + m;
+
+	if (j > size)
+		j = size;
+
+	for (i; i < j; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+
+	return (-1);
+}
