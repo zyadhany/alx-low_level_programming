3534c4b0661126f90bfc30ac8d4376cc768e40b7
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-15 21:28:59 +0000

    more pointer

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,17 @@ char *cap_string(char *str)
 		{
 			str[i] -= 32;
 		}
-
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+			str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+			str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+			str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+			str[i] == '}')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			{
+				str[i + 1] -= 32;
+			}
+		}
 		i++;
 	}
 
