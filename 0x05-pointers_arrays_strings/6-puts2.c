1d1af46c9d7704d72aa6cabc624c3779584facf0
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-15 20:39:17 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first
+ * @str: pointer to the string to print
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
