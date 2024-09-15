98c6f33d2d222e83894bf19a9be44b4a1fd222d1
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-15 21:33:32 +0000

    more pointer

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+
+/**
+ * magic - adds two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the sum of a and b
+ */
+int magic(int a, int b)
+{
+	return (a + b);
+}
