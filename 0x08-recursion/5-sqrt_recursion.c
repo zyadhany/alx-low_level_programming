a47c3964a6ab44f69313fc00ed30ba61b0cfb44e
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-16 14:37:11 +0000

    recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,18 +2,19 @@
 
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * get_sqrt - returns the natural square root of a number
  * @n: number to get square root of
+ * @low: low end of search range
+ * @high: high end of search range
  *
  * Return: square root of n
  */
-int _sqrt_recursion(int n)
-{
-	int low, high, mid;
 
-	low = 0;
-	high = n;
-	while (low <= high)
+int get_sqrt(int n, int low, int high)
+{
+	int mid;
+
+	if (low <= high)
 	{
 		mid = (low + high) / 2;
 		if (mid * mid == n)
@@ -22,13 +23,24 @@ int _sqrt_recursion(int n)
 		}
 		else if (mid * mid < n)
 		{
-			low = mid + 1;
+			return (get_sqrt(n, mid + 1, high));
 		}
 		else
 		{
-			high = mid - 1;
+			return (get_sqrt(n, low, mid - 1));
 		}
 	}
-
 	return (-1);
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to get square root of
+ *
+ * Return: square root of n
+ */
+int _sqrt_recursion(int n)
+{
+	return (get_sqrt(n, 0, n));
 }
