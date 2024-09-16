182268691c95a7b63403748b12844df71c464664
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-16 14:54:08 +0000

    recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,9 +22,22 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s2 == '*')
 	{
-		sol |= wildcmp(s1, s2 + 1);
-		sol |= wildcmp(s1 + 1, s2);
-		sol |= wildcmp(s1 + 1, s2 + 1);
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else if (*(s2 + 1) == '\0')
+		{
+			return (1);
+		}
+		else if (*s1 == '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		else
+		{
+			return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+		}
 	}
 
 	return (sol);
