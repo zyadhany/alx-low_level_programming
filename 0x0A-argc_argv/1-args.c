24d10f0729c2f22cb7ccfed2283f9e57893f3f3e
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-16 16:13:37 +0000

    more pointer

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%i\n", argc);
+	return (0);
+}
