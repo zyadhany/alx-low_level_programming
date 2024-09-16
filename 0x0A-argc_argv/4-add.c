d0af11b847e0085b701d00b45c53cc62bdaaca0b
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-16 16:38:27 +0000

    more pointer

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 + num2;
+
+	printf("%d\n", result);
+
+	return (0);
+}
