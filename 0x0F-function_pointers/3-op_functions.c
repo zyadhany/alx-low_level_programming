316daf905ecf2c697d86e9746d1c12d99bfc39df
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-17 17:22:18 +0000

    more pointer

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -41,6 +41,10 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0) {
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -52,5 +56,9 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0) {
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
