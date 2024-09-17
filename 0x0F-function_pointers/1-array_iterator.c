746ed1d388d07ac724e6676ee0da24fb2b720591
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-17 14:09:05 +0000

    func pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+  #include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given
+ * @array: array to iterate through
+ * @size: size of the array
+ * @action: pointer to the function to use
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
