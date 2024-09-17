4de8cafc0b05e5f253949e9d28948fa91ea3d84d
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-17 14:05:32 +0000

    func pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+  #include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer to a function that prints a name
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
